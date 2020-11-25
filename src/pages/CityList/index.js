6ad5de56b442bc19d1dde7e3d65ac09896a238a3
liyiqiang <852427849@qq.com>
2020-11-25 10:39:58 +0800

    处理城市列表数据

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react'
 import { NavBar, Icon } from 'antd-mobile';
+import axios from 'axios'
 import './index.scss'
 
+function formatCityList(list) {
+
+    const cityList = {};
+
+    for (let i = 0; i < list.length; i++) {
+        let item = list[i];
+        let first = item.short.substr(0, 1);
+
+        if (cityList[first]) {
+            cityList[first].push(item);
+        } else {
+            cityList[first] = [item];
+        }
+    }
+
+    const cityIndex = Object.keys(cityList).sort();
+
+    return {
+        cityIndex,
+        cityList
+    };
+
+}
+
 export default class CityList extends Component {
+    componentDidMount() {
+        this.fetchCityList()
+    }
+
+    async fetchCityList() {
+        const res = await axios.get('http://localhost:8080/area/city?level=1')
+        const { cityList, cityIndex } = formatCityList(res.data.body)
+    }
     render() {
         return (
             <div className="city">
