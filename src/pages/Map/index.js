94e3dd95dabe2fdb90d7e1e7c38ea9ff4e071162
liyiqiang <852427849@qq.com>
2020-11-27 17:30:22 +0800

    地图当前城市信息获取和百度地图比例尺和缩放功能以及添加文字标记

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -10,17 +10,38 @@ export default class Map extends Component {
         map.centerAndZoom(new window.BMap.Point(116.404, 39.915), 11);
         // 创建地址解析器实例     
         var myGeo = new window.BMap.Geocoder();
-        // 将地址解析结果显示在地图上，并调整地图视野    
+        // 将地址解析结果显示在地图上，并调整地图视野   
         myGeo.getPoint(city.label, function (point) {
             if (point) {
                 map.centerAndZoom(point, 11);
                 map.addOverlay(new window.BMap.Marker(point));
+                //添加控件
+                map.addControl(new window.BMap.NavigationControl());
+                map.addControl(new window.BMap.ScaleControl());
+
+                var opts = {
+                    position: point, // 指定文本标注所在的地理位置
+                    offset: new window.BMap.Size(30, -30) // 设置文本偏移量
+                };
+                // 创建文本标注对象
+                var label = new window.BMap.Label('你好', opts);
+                // 自定义文本标注样式
+                label.setStyle({
+                    color: 'red',
+                    borderRadius: '5px',
+                    borderColor: '#ccc',
+                    fontSize: '16px',
+                    height: '30px',
+                    lineHeight: '30px',
+                    fontFamily: '微软雅黑',
+                    display: 'inline-block',
+                    verticalAlign: 'middle'
+
+                });
+                map.addOverlay(label);
             }
         }, city.label);
 
-        // var map = new window.BMap.Map("container");
-        // var point = new window.BMap.Point(116.404, 39.915);
-        // map.centerAndZoom(point, 15);
     }
     render() {
         return (
