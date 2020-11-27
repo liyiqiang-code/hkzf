f58b777e594f862a171f05860e3cea15cf9eef32
liyiqiang <852427849@qq.com>
2020-11-27 15:02:52 +0800

    组件优化

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { NavBar, Icon } from 'antd-mobile';
+import propTypes from 'prop-types'
 import '../NavHeader/index.scss'
 
 function NavHeader(props) {
+    const defaultHandle = () => props.history.go(-1)
     return (
         <NavBar
             mode="light"
             icon={<Icon type="left" />}
-            onLeftClick={() => props.history.go(-1)}
+            onLeftClick={props.onLeftClick || defaultHandle}
         >{props.children}
         </NavBar>
     )
 }
+NavHeader.propTypes = {
+    children: propTypes.string.isRequired,
+    onLeftClick: propTypes.func
+}
+
 export default withRouter(NavHeader)
